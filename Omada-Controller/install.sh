0b0e2164e295e0f83137cae59ab9974b0aaead03
K4rl1Hub <47773218+K4rl1Hub@users.noreply.github.com>
2023-05-24 10:22:46 +0200

    Update install.sh

--- Omada-Controller/install.sh
+++ Omada-Controller/install.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+# omada controller dependency and package installer script for versions 4.x and 5.x
+
+# set default variables
 OMADA_DIR="/data/omada_controller"
 ARCH="${ARCH:-}"
 INSTALL_VER="${INSTALL_VER:-}"
-OMADA_VER="${OMADA_VER:-}"
-OMADA_TAR="${OMADA_TAR:-}"
-OMADA_URL="${OMADA_URL:-}"
+
+# install wget
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install --no-install-recommends -y ca-certificates wget
 
 # get URL to package based on major.minor version; for information on this url API, see https://github.com/mbentley/docker-omada-controller-url
 OMADA_URL="$(wget -q -O - "https://omada-controller-url.mbentley.net/hooks/omada_ver_to_url?omada-ver=${INSTALL_VER}")"
@@ -25,17 +30,19 @@ OMADA_VER="$(echo "${OMADA_TAR}" | awk -F '_v' '{print $2}' | awk -F '_' '{print
 OMADA_MAJOR_VER="${OMADA_VER%.*.*}"
 OMADA_MAJOR_MINOR_VER="${OMADA_VER%.*}"
 
+# function to exit on error w/message
 die() { echo -e "$@" 2>&1; exit 1; }
 
+echo "**** Selecting packages based on the architecture and version ****"
 # common package dependencies
 PKGS=(
   gosu
   net-tools
-  openjdk-8-jre-headless
   tzdata
   wget
 )
 
+# add specific package for mongodb
 case "${ARCH}" in
 amd64|arm64|"")
   PKGS+=( mongodb-server-core )
@@ -48,14 +55,47 @@ armv7l)
   ;;
 esac
 
+# add specific package for openjdk
+case "${ARCH}" in
+  amd64|arm64|"")
+    # use openjdk-17 for v5.4 and above; all others us openjdk-8
+    case "${OMADA_MAJOR_VER}" in
+      5)
+        # pick specific package based on the major.minor version
+        case "${OMADA_MAJOR_MINOR_VER}" in
+          5.0|5.1|5.3)
+            # 5.0 to 5.3 all use openjdk-8
+            PKGS+=( openjdk-8-jre-headless )
+            ;;
+          *)
+            # starting with 5.4, openjdk-17 is supported
+            PKGS+=( openjdk-17-jre-headless )
+            ;;
+        esac
+        ;;
+      *)
+        # all other versions, use openjdk-8
+        PKGS+=( openjdk-8-jre-headless )
+        ;;
+    esac
+    ;;
+  armv7l)
+    # always use openjdk-8 for armv7l
+    PKGS+=( openjdk-8-jre-headless )
+    ;;
+  *)
+    die "${ARCH}: unsupported ARCH"
+    ;;
+esac
+
+# output variables/selections
 echo "ARCH=${ARCH}"
 echo "OMADA_VER=${OMADA_VER}"
 echo "OMADA_TAR=${OMADA_TAR}"
 echo "OMADA_URL=${OMADA_URL}"
+echo "PKGS=( ${PKGS[*]} )"
 
 echo "**** Install Dependencies ****"
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
 apt-get install --no-install-recommends -y "${PKGS[@]}"
 
 echo "**** Download Omada Controller ****"
